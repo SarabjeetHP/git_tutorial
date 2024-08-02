055f863a232a16eb91a620bf8d3726d3c333e159
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-02 11:15:59 +0530

    Changes in Assignment

--- jquery.js
+++ jquery.js
@@ -1,1 +1,14 @@
-console.log("yoyo");
+$(document).ready(function () {
+  console.log("document loaded");
+});
+
+$(window).on("load", function () {
+  console.log("window loaded");
+});
+
+// window.onload = function () {
+//   alert("Lets Test");
+// };
+$(document).ready(function () {
+  alert("Lets Test Jquery");
+});
