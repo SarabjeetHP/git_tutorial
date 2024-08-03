1eb46f0f07b227eb4e5afbdb6b91763573dfd05f
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-03 11:57:35 +0530

    styling

--- Assignment4/index.js
+++ Assignment4/index.js
@@ -1,23 +1,23 @@
-$("#manage-permission").change(function () {
-  var value = $(this).val();
-  if (value === "") {
-    $(".hidden").show();
-  } else {
-    $(".hidden").hide();
-    $("#" + value + "-list").show();
-  }
-});
+// $("#manage-permission").change(function () {
+//   var value = $(this).val();
+//   if (value === "") {
+//     $(".hidden").show();
+//   } else {
+//     $(".hidden").hide();
+//     $("#" + value + "-list").show();
+//   }
+// });
 
-function toggleChildren(parent) {
-  $(parent).siblings("div").find(".child").prop("checked", parent.checked);
-}
+// function toggleChildren(parent) {
+//   $(parent).siblings("div").find(".child").prop("checked", parent.checked);
+// }
 
-function checkParent() {
-  $(".child").each(function () {
-    var parent = $(this).closest("div").siblings("input");
-    var allChecked =
-      $(this).siblings(".child:checked").length ===
-      $(this).siblings(".child").length;
-    parent.prop("checked", allChecked);
-  });
-}
+// function checkParent() {
+//   $(".child").each(function () {
+//     var parent = $(this).closest("div").siblings("input");
+//     var allChecked =
+//       $(this).siblings(".child:checked").length ===
+//       $(this).siblings(".child").length;
+//     parent.prop("checked", allChecked);
+//   });
+// }
