870d64b3e59629a026094eaa0e0e76961119f43c
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-03 17:38:54 +0530

    final Assignment

--- Assignment4/index.js
+++ Assignment4/index.js
@@ -1,5 +1,6 @@
 // $("#manage-permission").change(function () {
 //   var value = $(this).val();
+
 //   if (value === "") {
 //     $(".hidden").show();
 //   } else {
@@ -8,16 +9,20 @@
 //   }
 // });
 
-// function toggleChildren(parent) {
-//   $(parent).siblings("div").find(".child").prop("checked", parent.checked);
-// }
+function AllSelect(parent) {
+  $(parent).siblings("div").find(".child").prop("checked", parent.checked);
+}
 
-// function checkParent() {
-//   $(".child").each(function () {
-//     var parent = $(this).closest("div").siblings("input");
-//     var allChecked =
-//       $(this).siblings(".child:checked").length ===
-//       $(this).siblings(".child").length;
-//     parent.prop("checked", allChecked);
-//   });
-// }
+function checkParent() {
+  $(".child").each(function () {
+    var parent = $(this).closest("div").siblings("input");
+    var allChecked =
+      $(this).siblings(".child:checked").length === //current HTML element will be selected
+      $(this).siblings(".child").length;
+    parent.prop("checked", allChecked);
+  });
+}
+
+function Hide() {
+  $(".hidden").toggle(1000);
+}
--- Assignment5/index.js
+++ Assignment5/index.js
@@ -1,55 +1,55 @@
-// document.getElementById("add-another").addEventListener("click", function () {
-//   const container = document.getElementById("container");
-//   const newDiv = document.createElement("div");
-//   newDiv.classList.add("browser-container");
+document.getElementById("add-another").addEventListener("click", function () {
+  const container = document.getElementById("container");
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("browser-container");
 
-//   newDiv.innerHTML = `
-//         <input type="file" accept="image">
-//         <select class="image-type">
-//             <option value="primary">Primary Image</option>
-//             <option value="secondary">Secondary Image</option>
-//         </select>
-//         <span class="remove-link">Remove</span>
-//     `;
+  newDiv.innerHTML = `
+        <input type="file" accept="image">
+        <select class="image-type">
+            <option value="primary">Primary Image</option>
+            <option value="secondary">Secondary Image</option>
+        </select>
+        <span class="remove-link">Remove</span>
+    `;
 
-//   container.appendChild(newDiv);
+  container.appendChild(newDiv);
 
-//   newDiv.querySelector(".remove-link").addEventListener("click", function () {
-//     container.removeChild(newDiv);
-//     updatePrimaryOptions();
-//   });
+  newDiv.querySelector(".remove-link").addEventListener("click", function () {
+    container.removeChild(newDiv);
+    updatePrimaryOptions();
+  });
 
-//   newDiv
-//     .querySelector(".image-type")
-//     .addEventListener("change", updatePrimaryOptions);
-//   updatePrimaryOptions();
-// });
+  newDiv
+    .querySelector(".image-type")
+    .addEventListener("change", updatePrimaryOptions);
+  updatePrimaryOptions();
+});
 
-// function updatePrimaryOptions() {
-//   const selects = document.querySelectorAll(".image-type");
-//   const primaryCount = Array.from(selects).filter(
-//     (select) => select.value === "primary"
-//   ).length;
+function updatePrimaryOptions() {
+  const selects = document.querySelectorAll(".image-type");
+  const primaryCount = Array.from(selects).filter(
+    (select) => select.value === "primary"
+  ).length;
 
-//   selects.forEach((select) => {
-//     if (primaryCount >= 1 && select.value !== "primary") {
-//       select.querySelector('option[value="primary"]').disabled = true;
-//     } else {
-//       select.querySelector('option[value="primary"]').disabled = false;
-//     }
-//   });
-// }
+  selects.forEach((select) => {
+    if (primaryCount >= 1 && select.value !== "primary") {
+      select.querySelector('option[value="primary"]').disabled = true;
+    } else {
+      select.querySelector('option[value="primary"]').disabled = false;
+    }
+  });
+}
 
-// document.querySelectorAll(".remove-link").forEach((link) => {
-//   link.addEventListener("click", function () {
-//     const container = document.getElementById("container");
-//     container.removeChild(link.parentElement);
-//     updatePrimaryOptions();
-//   });
-// });
+document.querySelectorAll(".remove-link").forEach((link) => {
+  link.addEventListener("click", function () {
+    const container = document.getElementById("container");
+    container.removeChild(link.parentElement);
+    updatePrimaryOptions();
+  });
+});
 
-// document.querySelectorAll(".image-type").forEach((select) => {
-//   select.addEventListener("change", updatePrimaryOptions);
-// });
+document.querySelectorAll(".image-type").forEach((select) => {
+  select.addEventListener("change", updatePrimaryOptions);
+});
 
-// updatePrimaryOptions();
+updatePrimaryOptions();
