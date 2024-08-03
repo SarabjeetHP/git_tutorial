ddf33c0155f7f8335c4677530f63a3c7c161c804
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-03 11:33:10 +0530

    Assignment-7,Displaying data in the form of tables and columns

---
+++ Assignment7/index.js
@@ -0,0 +1,7 @@
+const arr = ["Sarabjeet", "Singh", "10/12/2001"];
+
+for (let i = 0; i < arr.length; i++) {
+  document.getElementById("arr1").textContent = arr[0];
+  document.getElementById("arr2").textContent = arr[1];
+  document.getElementById("arr3").textContent = arr[2];
+}
