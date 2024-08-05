fbaf8ecb21fdeb274d482400bed5d84fe4973f1e
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-05 15:40:24 +0530

    Assignment 5

--- Assignment5/index.js
+++ Assignment5/index.js
@@ -1,55 +1,72 @@
 document.getElementById("add-another").addEventListener("click", function () {
   const container = document.getElementById("container");
+
   const newDiv = document.createElement("div");
-  newDiv.classList.add("browser-container");
+
+  newDiv.className = "browser-container"; // Add the browser-container class to new div
 
   newDiv.innerHTML = `
-        <input type="file" accept="image">
-        <select class="image-type">
-            <option value="primary">Primary Image</option>
-            <option value="secondary">Secondary Image</option>
-        </select>
-        <span class="remove-link">Remove</span>
-    `;
+    <input type="file" accept="image/*" class="file-input">
+    <select class="image-type">
+        <option value="primary">Primary Image</option>
+        <option value="secondary" selected>Secondary Image</option>
+    </select>
+    <button class="remove-link">Remove</button>
+    <img src="" class="image-preview" />
+  `;
 
-  container.appendChild(newDiv);
+  container.appendChild(newDiv); // new Div created element will be stored at last
 
   newDiv.querySelector(".remove-link").addEventListener("click", function () {
     container.removeChild(newDiv);
-    updatePrimaryOptions();
   });
 
   newDiv
-    .querySelector(".image-type")
-    .addEventListener("change", updatePrimaryOptions);
-  updatePrimaryOptions();
+    .querySelector(".file-input")
+    .addEventListener("change", function (event) {
+      displayImage(event);
+    });
+
+  newDiv.querySelector(".image-type").addEventListener("change", function () {
+    updateImageTypes(this);
+  });
 });
 
-function updatePrimaryOptions() {
-  const selects = document.querySelectorAll(".image-type");
-  const primaryCount = Array.from(selects).filter(
-    (select) => select.value === "primary"
-  ).length;
+function displayImage(event) {
+  const input = event.target;
+  const file = input.files[0];
+  const reader = new FileReader();
 
-  selects.forEach((select) => {
-    if (primaryCount >= 1 && select.value !== "primary") {
-      select.querySelector('option[value="primary"]').disabled = true;
-    } else {
-      select.querySelector('option[value="primary"]').disabled = false;
-    }
-  });
+  reader.onload = function (e) {
+    const image =
+      input.nextElementSibling.nextElementSibling.nextElementSibling;
+    image.src = e.target.result;
+    image.style.display = "block";
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 }
 
-document.querySelectorAll(".remove-link").forEach((link) => {
-  link.addEventListener("click", function () {
-    const container = document.getElementById("container");
-    container.removeChild(link.parentElement);
-    updatePrimaryOptions();
+function updateImageTypes(changedSelect) {
+  if (changedSelect.value === "primary") {
+    document.querySelectorAll(".image-type").forEach((select) => {
+      if (select !== changedSelect) {
+        select.value = "secondary";
+      }
+    });
+  }
+}
+
+document.querySelectorAll(".file-input").forEach((input) => {
+  input.addEventListener("change", function (event) {
+    displayImage(event);
   });
 });
 
 document.querySelectorAll(".image-type").forEach((select) => {
-  select.addEventListener("change", updatePrimaryOptions);
+  select.addEventListener("change", function () {
+    updateImageTypes(this);
+  });
 });
-
-updatePrimaryOptions();
